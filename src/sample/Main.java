28c7b079f1ad0c53cae7f3eda57c2031d976c393
lemonPenguin <zossia.b1@gmail.com>
2020-02-19 19:18:38 +0100

    Little update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.io.FileNotFoundException;
 
 public class Main extends Application {
 
@@ -16,9 +20,21 @@ public class Main extends Application {
         stage = primaryStage;
         stage.setTitle("Życie Mateusza");
 
+        draw();
+    }
+
+    public void draw() throws FileNotFoundException {
         GridPane root = new GridPane();
 
-        Scene scene = new Scene(root,800,700);
+        for (int y = 0; y < 7; y++) {
+            for (int x = 0; x < 8; x++) {
+                Rectangle rectangle = new Rectangle(100,100, Color.LIGHTGRAY);
+                rectangle.setStroke(Color.DARKGREY);
+                root.add(rectangle,x,y);
+            }
+        }
+
+        Scene scene = new Scene(root,807,706);
         stage.setScene(scene);
         stage.show();
     }
